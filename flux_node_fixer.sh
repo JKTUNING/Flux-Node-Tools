e74684126f909f2e56b4f0441829925f22542ca6
JKTUNING <jktuned.crypto@gmail.com>
2022-08-19 18:08:57 -0400

    add simple curses gui details

--- flux_node_fixer.sh
+++ flux_node_fixer.sh
@@ -21,7 +21,7 @@ BENCH_DIR_LOG='.fluxbenchmark'
 #gets fluxbench version info
 flux_bench_version=$(($BENCH_CLI getinfo) | jq -r '.version')
 
-#gets fluxbench status
+gets fluxbench status
 flux_bench_details=$($BENCH_CLI getstatus)
 flux_bench_back=$(jq -r '.flux' <<<"$flux_bench_details")
 flux_bench_flux_status=$(jq -r '.status' <<<"$flux_bench_details")
@@ -48,85 +48,85 @@ flux_node_last_paid_height=$(jq -r '.last_paid_height' <<<"$flux_node_details")
 #calculated block height since last confirmed
 blockDiff=$(($flux_daemon_block_height-$flux_node_last_confirmed_height))
 
-function check_status() {
-  if [[ $flux_bench_flux_status == "online" ]];
-  then
-    echo -e "Flux node status           -    ${GREEN}ONLINE${NC}"
-  else
-    echo -e "Flux node status           -    ${RED}OFFLINE${NC}"
-  fi
-}
+# function check_status() {
+#   if [[ $flux_bench_flux_status == "online" ]];
+#   then
+#     echo -e "Flux node status           -    ${GREEN}ONLINE${NC}"
+#   else
+#     echo -e "Flux node status           -    ${RED}OFFLINE${NC}"
+#   fi
+# }
 
-function check_bench() {
-  if [[ ($flux_bench_benchmark == "failed") || ($flux_bench_benchmark == "toaster") ]]; then
-    echo -e "Flux node benchmark        -    ${RED}$flux_bench_status${NC}"
-    read -p 'would you like to check for updates and restart benchmarks? (y/n) ' userInput
-    if [ $userInput == 'n' ]; then
-      echo 'user does not want to restart benchmarks'
-    else
-      echo 'user would like to restart benchmarks'
-      flux_update_benchmarks
-    fi
-  elif [[ $flux_bench_benchmark == "running" ]]; then
-    echo -e "${BLUE}node benchmarks running ... ${NC}"
-  elif [[ $flux_bench_benchmark == "dos" ]]; then
-    echo -e "${RED}node in denial of service state${NC}"
-  else
-    echo -e "Flux node benchmark        -    ${GREEN}$flux_bench_benchmark${NC}"
-  fi
-}
+# function check_bench() {
+#   if [[ ($flux_bench_benchmark == "failed") || ($flux_bench_benchmark == "toaster") ]]; then
+#     echo -e "Flux node benchmark        -    ${RED}$flux_bench_status${NC}"
+#     read -p 'would you like to check for updates and restart benchmarks? (y/n) ' userInput
+#     if [ $userInput == 'n' ]; then
+#       echo 'user does not want to restart benchmarks'
+#     else
+#       echo 'user would like to restart benchmarks'
+#       flux_update_benchmarks
+#     fi
+#   elif [[ $flux_bench_benchmark == "running" ]]; then
+#     echo -e "${BLUE}node benchmarks running ... ${NC}"
+#   elif [[ $flux_bench_benchmark == "dos" ]]; then
+#     echo -e "${RED}node in denial of service state${NC}"
+#   else
+#     echo -e "Flux node benchmark        -    ${GREEN}$flux_bench_benchmark${NC}"
+#   fi
+# }
 
-function check_back(){
-  if [[ $flux_bench_back != *"connected"* ]];
-  then
-    echo -e "Flux back status           -    ${RED}DISCONNECTED${NC}"
-    read -p 'would you like to check for updates and restart flux-back? (y/n) ' userInput
-    if [ $userInput == 'n' ]; then
-      echo -e "${RED}user does not want to restart flux back${NC}"
-    else
-      echo -e "${BLUE}user would like to update and restart flux-back${NC}"
-      echo 'updating ... '
-      flux_update_restart
-    fi
-  else
-    echo -e "Flux back status           -    ${GREEN}CONNECTED${NC}"
-  fi
-}
+# function check_back(){
+#   if [[ $flux_bench_back != *"connected"* ]];
+#   then
+#     echo -e "Flux back status           -    ${RED}DISCONNECTED${NC}"
+#     read -p 'would you like to check for updates and restart flux-back? (y/n) ' userInput
+#     if [ $userInput == 'n' ]; then
+#       echo -e "${RED}user does not want to restart flux back${NC}"
+#     else
+#       echo -e "${BLUE}user would like to update and restart flux-back${NC}"
+#       echo 'updating ... '
+#       flux_update_restart
+#     fi
+#   else
+#     echo -e "Flux back status           -    ${GREEN}CONNECTED${NC}"
+#   fi
+# }
 
-function node_os_update(){
-  sudo apt-get --with-new-pkgs upgrade -y && sudo apt autoremove -y
-}
+# function node_os_update(){
+#   sudo apt-get --with-new-pkgs upgrade -y && sudo apt autoremove -y
+# }
 
-function flux_update_service(){
-  node_os_update
-  #sudo systemctl stop flux
-  #sleep 2
-  #sudo systemctl start flux
-  #sleep 5
-}
+# function flux_update_service(){
+#   node_os_update
+#   #sudo systemctl stop flux
+#   #sleep 2
+#   #sudo systemctl start flux
+#   #sleep 5
+# }
 
-function flux_update_benchmarks(){
-  node_os_update
-  #$BENCH_CLI restartnodebenchmarks
-}
+# function flux_update_benchmarks(){
+#   node_os_update
+#   #$BENCH_CLI restartnodebenchmarks
+# }
 
-function flux_daemon_info(){
-  echo -e "$BLUE_CIRCLE   Flux daemon version          -    $flux_daemon_version"
-  echo -e "$BLUE_CIRCLE   Flux protocol version        -    $flux_daemon_protocol_version"
-  echo -e "$BLUE_CIRCLE   Flux daemon block height     -    $flux_daemon_block_height"
-  echo -e "$BLUE_CIRCLE   Flux daemon connections      -    $flux_daemon_connections"
-  echo -e "$BLUE_CIRCLE   Flux deamon difficulty       -    $flux_daemon_difficulty"
-}
+# function flux_daemon_info(){
+#   echo -e "$BLUE_CIRCLE   Flux daemon version          -    $flux_daemon_version"
+#   echo -e "$BLUE_CIRCLE   Flux protocol version        -    $flux_daemon_protocol_version"
+#   echo -e "$BLUE_CIRCLE   Flux daemon block height     -    $flux_daemon_block_height"
+#   echo -e "$BLUE_CIRCLE   Flux daemon connections      -    $flux_daemon_connections"
+#   echo -e "$BLUE_CIRCLE   Flux deamon difficulty       -    $flux_daemon_difficulty"
+# }
 
-function flux_node_info(){
-  echo -e "$BLUE_CIRCLE   Flux node status             -    $flux_node_status"
-  echo -e "$BLUE_CIRCLE   Flux node collateral         -    $flux_node_collateral"
-  echo -e "$BLUE_CIRCLE   Flux node added height       -    $flux_node_added_height"
-  echo -e "$BLUE_CIRCLE   Flux node confirmed height   -    $flux_node_confirmed_height"
-  echo -e "$BLUE_CIRCLE   Flux node last confirmed     -    $flux_node_last_confirmed_height"
-  echo -e "$BLUE_CIRCLE   Flux node last paid height   -    $flux_node_last_paid_height"
-  echo -e "$BLUE_CIRCLE   Blocks since last confirmed  -    $blockDiff"
-}
+# function flux_node_info(){
+#   echo -e "$BLUE_CIRCLE   Flux node status             -    $flux_node_status"
+#   echo -e "$BLUE_CIRCLE   Flux node collateral         -    $flux_node_collateral"
+#   echo -e "$BLUE_CIRCLE   Flux node added height       -    $flux_node_added_height"
+#   echo -e "$BLUE_CIRCLE   Flux node confirmed height   -    $flux_node_confirmed_height"
+#   echo -e "$BLUE_CIRCLE   Flux node last confirmed     -    $flux_node_last_confirmed_height"
+#   echo -e "$BLUE_CIRCLE   Flux node last paid height   -    $flux_node_last_paid_height"
+#   echo -e "$BLUE_CIRCLE   Blocks since last confirmed  -    $blockDiff"
+# }
 
 #flux_daemon_info
 #flux_node_info
@@ -136,13 +136,29 @@ function flux_node_info(){
 
 main (){
     #basic information, hostname, date, ...
-    window "`hostname`" "red"
-    append "`date`"
-    addsep
-    append_tabbed "Up since|`uptime | cut -f1 -d"," | sed 's/^ *//' | cut -f3- -d" "`" 2 "|"
-    append_tabbed "Users:`uptime | cut -f2 -d"," | sed 's/^ *//'| cut -f1 -d" "`" 2
-    append_tabbed "`awk '{print "Load average:" $1 " " $2 " " $3}' < /proc/loadavg`" 2
+    window "Flux Daemon Details" "blue" "50%"
+      append "`date`"
+      addsep
+      append_tabbed "Flux daemon version:$flux_daemon_version" 2
+      append_tabbed "Flux protocol version:$flux_daemon_protocol_version"  2
+      append_tabbed "Flux protocol block height:$flux_daemon_block_height"  2
+      append_tabbed "Flux protocol connections:$flux_daemon_connections"  2
+      append_tabbed "Flux protocol difficulty:$flux_daemon_difficulty"  2
+    endwin
+
+      col_right
+      move_up
+
+     #5 most used processes ordered by cpu and memory usage
+    window "Flux Node Details" "green" "50%"
+      append_tabbed "Flux node status:$flux_node_status"  2
+      append_tabbed "Flux collateral:$flux_node_collateral"  2
+      append_tabbed "Flux added height:$flux_node_added_height"  2
+      append_tabbed "Flux confirmed height:$flux_node_confirmed_height"  2
+      append_tabbed "Flux last confirmed:$flux_node_last_confirmed_height"  2
+      append_tabbed "Flux last paid height:$flux_node_last_paid_height"  2
+      append_tabbed "Blocks since last confirmed:$blockDiff"  2
     endwin
 }
 
-main_loop 10
+main_loop -t 1 "$@"
