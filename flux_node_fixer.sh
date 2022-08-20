dee110e3c6da34a8dea20208135192daf5ee6818
JKTUNING <jktuned.crypto@gmail.com>
2022-08-20 12:47:16 -0400

    check for jq package - redraw term resize

--- flux_node_fixer.sh
+++ flux_node_fixer.sh
@@ -2,6 +2,16 @@
 
 #source /dev/stdin <<<"$(curl -s https://raw.githubusercontent.com/JKTUNING/Flux-Node-Tools/main/simple_curses.sh)"
 
+if ! jq --version >/dev/null 2>&1; then
+  echo -e "${RED}jq not found ... installing jq${NC}"
+  sudo apt install jq -y
+
+  if ! jq --version >/dev/null 2>&1; then
+    echo "jq install was not successful - exiting"
+    exit
+  fi
+fi
+
 #colors
 GREEN='\033[1;32m'
 RED='\033[1;31m'
@@ -37,9 +47,13 @@ BENCH_LOG_DIR='benchmark_debug_error.log'
 DAEMON_LOG_DIR='flux_daemon_debug_error.log'
 WATCHDOG_LOG_DIR='~/watchdog/watchdog_error.log'
 
-show_daemon='0'
+#variables to draw windows
 show_bench='1'
+show_daemon='0'
 show_node='0'
+
+# variable to see if the terminal size has changed
+redraw_term='1'
 
 #gets fluxbench version info
 flux_bench_version=$(($BENCH_CLI getinfo) | jq -r '.version')
@@ -169,21 +183,26 @@ function update (){
     show_node='0'
     show_daemon='0'
     show_bench='1'
+    redraw_term='1'
     sleep 0.1
   elif [[ $userInput == 'n' ]]; then
     show_node='1'
     show_daemon='0'
     show_bench='0'
+    redraw_term='1'
     sleep 0.1
   elif [[ $userInput == 'd' ]]; then
     daemon_log=$(tail -5 $DAEMON_LOG_DIR)
     show_node='0'
     show_daemon='1'
     show_bench='0'
+    redraw_term='1'
     sleep 0.1
   elif [[ $userInput == 'q' ]]; then
     clear
     exit
+  else
+    redraw_term='0'
   fi
 }
 
@@ -358,20 +377,32 @@ function navigation(){
   echo -e "         d for daemon info | b for benchmarks | n for node | q to quit         " 
 }
 
+#checks the current window size and compares it to the last windows size to see if we need to redraw the term
+function check_term_resize(){
+  local currentWidth
+  currentWidth=$(tput cols)
+  if [[ $WINDOW_WIDTH -ne $currentWidth  ]]; then
+    redraw_term='1'
+  fi
+}
+
+
 function main_terminal(){
  
   while true; do
+    check_term_resize
+
     WINDOW_WIDTH=$(tput cols)
     WINDOW_HALF_WIDTH=$(bc <<<"$WINDOW_WIDTH / 2")
-    if [[ $show_daemon == '1' ]]; then
-      flux_daemon_info
-      show_daemon='0'
-    elif [[ $show_node == '1' ]]; then
-      flux_node_info
-      show_node='0'
-    elif [[ $show_bench == '1' ]]; then
-      flux_benchmark_info
-      show_bench='0'
+
+    if [[ $redraw_term == '1' ]]; then
+      if [[ $show_daemon == '1' ]]; then
+        flux_daemon_info
+      elif [[ $show_node == '1' ]]; then
+        flux_node_info
+      elif [[ $show_bench == '1' ]]; then
+        flux_benchmark_info
+      fi
     fi
     update
   done
