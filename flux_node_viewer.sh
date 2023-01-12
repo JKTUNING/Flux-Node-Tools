afc470df7d6271fdfaaee47a4b346973ce21d6e7
John K <john@j-k-tuning.com>
2023-01-11 22:43:36 -0500

    make navigation bar yellow

--- flux_node_viewer.sh
+++ flux_node_viewer.sh
@@ -1153,7 +1153,7 @@ function make_header(){
 #this function simply prints tile navigation at the bottom of the current tile
 function navigation(){
   make_header
-  echo -e "d - daemon | b - benchmarks | n - node | l - logs | q - quit | c - commands" 
+  echo -e "${YELLOW}d - daemon | b - benchmarks | n - node | l - logs | q - quit | c - commands${NC}" 
 }
 
 #this function simply prints the version at the top of the page
